765387128617a6168ebeca8d808d84b60bfffda7
Kasarapu Aravind <splendidaravind552@gmail.com>
2025-04-28 01:56:57 -0500

    eks , argocd code

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -52,7 +52,7 @@ resource "aws_eks_addon" "vpc_cni" {
     cluster_name = aws_eks_cluster.this.name
     addon_name = "vpc-cni"
     addon_version = var.vpc_cni_version
-    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "OVERWRITE"
     service_account_role_arn = null
 
     depends_on = [ aws_eks_cluster.this ]  
@@ -63,7 +63,7 @@ resource "aws_eks_addon" "coredns" {
     cluster_name = aws_eks_cluster.this.name
     addon_name = "coredns"
     addon_version = var.coredns_version
-    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "OVERWRITE"
 
     depends_on = [ aws_eks_cluster.this ]
 }
@@ -73,7 +73,7 @@ resource "aws_eks_addon" "kube_proxy" {
     cluster_name = aws_eks_cluster.this.name
     addon_name = "kube-proxy"
     addon_version = var.kube_proxy_version
-    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "OVERWRITE"
     
     depends_on = [ aws_eks_cluster.this ]
 }
@@ -83,7 +83,7 @@ resource "aws_eks_addon" "ebs_csi_driver" {
     cluster_name = aws_eks_cluster.this.name
     addon_name = "aws-ebs-csi-driver"
     addon_version = var.ebs_csi_driver_version
-    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "OVERWRITE"
     service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
 
     depends_on = [ aws_eks_cluster.this ]
